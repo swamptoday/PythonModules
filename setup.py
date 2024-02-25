c74c368f7e374d1e46e79c1b9c81ab829e951587
Maryna Nyzova <97362192+swamptoday@users.noreply.github.com>
2024-02-25 18:20:58 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MyModule',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List of dependencies
+    ],
+    # Other metadata such as author, description, etc.
+)
